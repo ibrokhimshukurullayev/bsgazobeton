8d73e8ddcdc7de67fe0120335eb3bfa431f5b640
Sokhib Jaynarov <sokhibjaynarov@gmail.com>
2025-08-14 12:17:29 +0500

    Update Next.js configuration for standalone output and enhance README with project overview, features, tech stack, and deployment instructions.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ["files.glotr.uz", "www.figma.com", "www.google.com", "i0.wp.com"],
   },
