697a2d8606f3e0c230da2638bf09538b18e5bcce
sokhibjaynarov <sokhibjaynarov@gmail.com>
2026-03-10 22:22:26 +0500

    base api changed

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export const API_ORIGIN =
-  process.env.NEXT_PUBLIC_API_ORIGIN || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_API_ORIGIN || "https://api.bsgazbeton.uz";
 
 export const API_BASE_URL = `${API_ORIGIN}/api`;
 
